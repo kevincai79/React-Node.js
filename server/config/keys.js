bc2f13a0fb1c41befce97228ae90b56af3aab079
Kevin Cai <kevincai1979@gmail.com>
2018-02-02 14:10:12 -0800

    set up cookie

--- server/config/keys.js
+++ server/config/keys.js
@@ -2,5 +2,6 @@ module.exports = {
   googleClientID:
     '282727270345-4v8p7rnvhplfvnv7tlatp1k42ooce833.apps.googleusercontent.com',
   googleClientSecret: 'h-OKxvyxYV85Jse9eIlSREbf',
-  mongoURI: 'mongodb://kevin:password@ds221228.mlab.com:21228/emaily-dev-2018'
+  mongoURI: 'mongodb://kevin:password@ds221228.mlab.com:21228/emaily-dev-2018',
+  cookieKey: 'eojdlsoeisoosoenkeozioezkjhjsowoowslsowowosolshfhueusuuweipa'
 };
