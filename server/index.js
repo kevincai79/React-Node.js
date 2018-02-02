079dfa98e4ce5a26b953f91287902688d9e71353
Kevin Cai <kevincai1979@gmail.com>
2018-02-01 19:26:32 -0800

    set up MongoDB

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 // import express library
 const express = require('express');
+const mongoose = require('mongoose');
 require('./services/passport');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
+
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
