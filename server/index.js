f119127c1620ac150642772fefad01982a12c778
Kevin Cai <kevincai1979@gmail.com>
2018-02-01 19:40:41 -0800

    create User model

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 // import express library
 const express = require('express');
 const mongoose = require('mongoose');
-require('./services/passport');
 const keys = require('./config/keys');
+require('./services/passport');
+require('./models/User');
 
 mongoose.connect(keys.mongoURI);
 
